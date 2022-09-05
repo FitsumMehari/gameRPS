3f9335259681548948363a809d1ea950e0e784cd
Fitsum Mehari <m.fitsummehari@gmail.com>
2022-09-05 22:33:06 +0300

    all done

---
+++ script.js
@@ -0,0 +1,80 @@
+$(document).ready(function () {
+    var yourScore = 0;
+    var compScore = 0;
+
+    var choices = ["Rock", "Paper", "Scissor"]
+
+    $('#your-score').append(yourScore);
+    $('#comp-score').append(compScore);
+
+    function logic(o1, o2) {
+        if (o1 === o2) {
+            return "tie";
+        }
+        if ((o1 === "Rock") && (o2 === "Scissor")) {
+            return "o1";
+        }
+        if ((o1 === "Rock") && (o2 === "Paper")) {
+            return "o2";
+        }
+        if ((o1 === "Paper") && (o2 === "Scissor")) {
+            return "o2";
+        }
+    }
+
+    $("#rock-btn").on('click', () => {
+        $("#your-choice").text('Rock');
+        $("#comp-choice").text(compTurn());
+
+        var winner = logic($("#your-choice").text(), $("#comp-choice").text());
+        setScores(winner);
+    })
+
+    $("#paper-btn").on('click', () => {
+        $("#your-choice").text('Paper');
+        $("#comp-choice").text(compTurn());
+
+        var winner = logic($("#your-choice").text(), $("#comp-choice").text());
+        setScores(winner);
+    })
+
+    $("#scs-btn").on('click', () => {
+        $("#your-choice").text('Scissor');
+        $("#comp-choice").text(compTurn());
+        
+        var winner = logic($("#your-choice").text(), $("#comp-choice").text());
+        setScores(winner);
+    })
+
+    $("#res-btn").on('click', () => {
+        window.location.reload();
+    })
+
+    function compTurn() {
+        // get random index value
+        const randomIndex = Math.floor(Math.random() * choices.length);
+
+        // get random item
+        const choice = choices[randomIndex];
+
+        return choice;
+
+    }
+
+    function setScores(winner) {
+        if (winner === 'tie') {
+            yourScore += 1;
+            compScore += 1;
+            $('#your-score').text(yourScore);
+            $('#comp-score').text(compScore);
+        } else if (winner === 'o1') {
+            yourScore += 1;
+            $('#your-score').text(yourScore);
+            $('#comp-score').text(compScore);
+        } else if (winner === 'o2') {
+            compScore += 1;
+            $('#your-score').text(yourScore);
+            $('#comp-score').text(compScore);
+        }
+    }
+});
